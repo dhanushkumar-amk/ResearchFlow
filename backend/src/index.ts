c8f8b46be2abdcafb2cff06bbe52e2ff1f400ca7
dhanushkumar <dhanushkumaramk@gmail.com>
2026-03-23 21:03:04 +0530

    chore: Install backend Node.js dependencies.

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server running');
+});
+
+app.listen(port, () => {
+  console.log(`➡️ Server running on port ${port}`);
+});
